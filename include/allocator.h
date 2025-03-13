8c20ec466e309db59aa3f5ba27329ad6296d0610
Chisheng Chen <johnny1001s000602@gmail.com>
2025-03-13 11:04:59 +0800

    Add interfaces and structure definitions
    Interfaces and structure definitions of the allocator was specified in
    allocator.h.

---
+++ include/allocator.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ALIGN_UP(size) (((size) + 7) & ~0x7)
+#define PREV_INUSE(block) (((block_t *)(block))->prev_size & 1)
+#define INUSE(block) (((block_t *)(block))->size & 1)
+
+
+typedef struct block {
+    size_t size;
+    size_t prev_size;
+    struct block *l, *r;
+    char mem[0];
+} block_t;
+
+typedef struct allocator {
+    size_t size;
+    block_t *root;
+    char mem[0];
+}allocator_t;
+
+/**
+ * alloc_create - Create a new allocator with a
+ * maximum memory size, which will be aligned up to 8 bytes
+ * @size: maximum memory size
+ */
+allocator_t *alloc_create(size_t size);
+
+/**
+ * print_tree - Print the tree in preorder
+ * @root: the root node
+ */
+void print_tree(block_t *root);
