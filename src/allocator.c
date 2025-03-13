a58cf963daffa6a9939899e393ba92730148fef3
Chisheng Chen <johnny1001s000602@gmail.com>
2025-03-13 11:14:37 +0800

    Add find_node to allocator.c
    This function can find who points to the  node with
    a specific address.

--- src/allocator.c
+++ src/allocator.c
@@ -22,3 +22,23 @@ void print_tree(block_t *root)
     printf("%ld ", root->size);
     print_tree(root->r);
 }
+
+static block_t **find_node(block_t **root, block_t *target)
+{
+    while (*root) {
+        if ((*root)->l &&
+            (((*root)->l->size > target->size) ||
+             (((*root)->l->size == target->size) && ((*root)->l >= target))))
+            root = &(*root)->l;
+        else if ((*root)->size > target->size ||
+                 (((*root)->size == target->size) && (*root >= target)))
+            break;
+        else
+            root = &(*root)->r;
+    }
+
+    if (!(*root) || (*root != target))
+        return NULL;
+
+    return root;
+}
